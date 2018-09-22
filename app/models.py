50c670fad653d8d1503830e0f23692efbdaab186
Amit Kumar. Kataria <C5079832@acsind.com>
2018-09-22 13:51:40 +0530

    Password has generation in models and flask shell context setting in microblog

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from app import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     # __tablename__ = 'USER'
@@ -19,6 +21,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash,password)
 
 
 # It is an unfortunate inconsistency that in some instances such as in a db.relationship() call, the model is
