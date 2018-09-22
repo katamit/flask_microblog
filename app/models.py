1e3ca4d0ea5b926e34433e7dfaac940ae989b114
Amit Kumar. Kataria <C5079832@acsind.com>
2018-09-22 10:28:33 +0530

    added Post to models and post table to database

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 
 
 class User(db.Model):
+    # __tablename__ = 'USER'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -27,7 +28,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.Foreign_key('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}'.format(self.body)
+        return '<Post {}>'.format(self.body)
