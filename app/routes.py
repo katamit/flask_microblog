4def04bbcfc9000c3ab62c45aea5891b82afaab5
Amit Kumar. Kataria <C5079832@acsind.com>
2018-09-25 00:27:16 +0530

    user registration is enabled and saved in db

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from werkzeug.urls import url_parse
 
-from app import application
+from app import application, db
 from flask import render_template, flash, redirect, url_for, request
-from app import forms
 from flask_login import current_user, login_user, logout_user, login_required
 
+from app import forms
+from app.forms import RegistrationForm
 from app.models import User
 
 
@@ -51,3 +52,28 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@application.route('/register', methods=['GET', 'Post'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulation,You are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@application.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post#1'},
+        {'author': user, 'body': 'Test post#2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
