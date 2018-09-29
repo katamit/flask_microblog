f55b0d19788384942919dd9662f417b3b55d8c14
Amit Kumar Kataria <Amitkumar_Kataria@ad.infosys.com>
2018-09-29 13:14:04 +0530

    Enabled error logging - configured mail also

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -12,5 +15,22 @@ Migrate = Migrate(application, db)
 
 login = LoginManager(application)
 login.login_view = 'login'
+
+if not application.debug:
+    if application.config['MAIL_SERVER']:
+        auth = None
+        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
+        secure = None
+        if application.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs=application.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        application.logger.addHandler(mail_handler)
+
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -1,12 +1,25 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
-    '''
-    As the application needs more configuration items, they can be added to this class, and later if I find that I need to have more than one configuration set, I can create subclasses of it.'''
+    """
+    As the application needs more configuration items, they can be added to this class, and later if
+    I find that I need to have more than one configuration set, I can create subclasses of it.
+    """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    #.., configurtin fo 
+    # configuration for database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # config for Mail server
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['senior_thomas@yahoo.in']
+
 
